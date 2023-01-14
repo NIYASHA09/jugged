549783b498dd009a89880866138085aaf8c8168d
Niyasha Jain <115873341+NIYASHA09@users.noreply.github.com>
2023-01-14 14:27:13 +0530

    Add files via upload

---
+++ firstcss.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+int area(int length,int breadth)
+{
+    return length*breadth;
+}
+int perimeter(int length, int breadth)
+{
+    return 2*(length+breadth);
+}
+ int main(){
+     int length=0,breadth=0;
+     printf("enter length and breadth");
+     cin>>length>>breadth;
+      
+     int a= area(length,breadth);
+     int peri= perimeter(length,breadth);
+     printf("area=%d\nperi=%d\n",a,peri);
+     return 0;
+ }
